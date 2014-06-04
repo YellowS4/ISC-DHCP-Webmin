6c8b037a17d81508f708744f386e922d4e1fea08
florianaudon <florianaudon@users.noreply.github.com>
2014-06-04 15:08:54 +0200

    Update modif_conf.php

--- inc/modif_conf.php
+++ inc/modif_conf.php
@@ -2,14 +2,14 @@
 <?php
 //Includes
 include 'inc/fonctionsBDD.php';
-$connexionBDD=connexionBDD();
+$connex=connexionBDD();
 include 'inc/network.php';
 //Constantes
 define("NB_CONF_PAGE","2");
 define("NB_PAGE","2");
 $nw=get_network();
 //On securise la variable ID
-if(isset($_GET['id'])){
+if(isset($_GET['id']) && $_GET['id']>=0){
 	settype($_GET['id'],"string");
 	$id=$_GET['id'];
 }else{
@@ -22,7 +22,7 @@ if(isset($_GET['id'])){
 		if($_POST['default']=="true"){
 			$resultats=$connex->query("SELECT id FROM dhcp_test WHERE conf_default=TRUE");
 			if($resultats->rowCount()>0){
-				$erreur.="Impossible, il existe d�j� une configuration par d�faut\n";
+				$erreur.="Impossible, il existe d?j? une configuration par d?faut\n";
 			}
 		}
 		if(isset($erreur) && $erreur!=""){
@@ -40,11 +40,11 @@ if(isset($_GET['id'])){
 		if($nb_conf%NB_CONF_PAGE==0){
 			$nb_pages=$nb_conf/NB_CONF_PAGE;
 		}else{
-			$nb_pages=floor($nb_conf/NB_CONF_PAGE)+1;
+			$nb_pages=floor($nb_conf/NB_CONF_PAGE);
 		}
 		$resultats=$connex->query("SELECT conf_contenu, interface, nom_conf, date_creation, conf_default, conf_actuelle  FROM dhcp_test LIMIT ".NB_CONF_PAGE." OFFSET ".$id*NB_CONF_PAGE."");
 	?>
-	Afficher seulement: <label>Tous <input type="checkbox" id="selection_conf" name="affichage[]" value="Tous" onclick="selection('conf')" checked="checked"></label> <label>La configuration par defaut <input type="checkbox" id="selection_default" name="affichage[]" value="Configuration_d�faut" onclick="selection('default')"></label> <label>La configuration actuelle <input type="checkbox" id="selection_actuelle" name="affichage[]" value="Configuration_actuelle" onclick="selection('actuelle')"></label> Concernant les interfaces:.... <br />
+	Afficher seulement: <label>Tous <input type="checkbox" id="selection_conf" name="affichage[]" value="Tous" onclick="selection('conf')" checked="checked"></label> <label>La configuration par defaut <input type="checkbox" id="selection_default" name="affichage[]" value="Configuration_d?faut" onclick="selection('default')"></label> <label>La configuration actuelle <input type="checkbox" id="selection_actuelle" name="affichage[]" value="Configuration_actuelle" onclick="selection('actuelle')"></label> Concernant les interfaces:.... <br />
 	<?php	
 		$i=0;
 		foreach ($resultats as $row) {	
@@ -76,7 +76,7 @@ if(isset($_GET['id'])){
 								if($row['interface']==$nom_inter){
 									$select='selected="selected"';
 								}
-								print '<option value="'.$nom_inter.'" '.$select.'>'.$nom_inter.' (Aucun adresse d�finie)</option>';
+								print '<option value="'.$nom_inter.'" '.$select.'>'.$nom_inter.' (Aucun adresse d?finie)</option>';
 							}
 							
 						} 
@@ -98,23 +98,25 @@ if(isset($_GET['id'])){
 		
 		//ESSAI DE PAGINATION
 		//On definit les pages a afficher
-		if(NB_PAGE>$nb_pages){
-			$page_max=$nb_pages;
-		}else{
-			$page_max=NB_PAGE;
-		}
 			//Le debut
-			for($i=0; $i<$page_max; $i++){
-				//On selectionne tous les parametre dans l'url
-					echo '<a href="index.php?page=modif_conf&id='.$i.'">'.$i.'</a>';
+			echo '<a href="index.php?page=modif_conf&id=0"><< </a>';
+			if($id>=6){
+				for($i=-NB_PAGE/2+5; $i<NB_PAGE/2+1+5; $i++){
+					//On selectionne tous les parametre dans l'url
+						echo '<a href="index.php?page=modif_conf&id='.$i.'">'.$i.'</a>';
+				}
+			}elseif($id<=0){
+				for($i=-NB_PAGE/2+1; $i<NB_PAGE/2+1+1; $i++){
+					//On selectionne tous les parametre dans l'url
+						echo '<a href="index.php?page=modif_conf&id='.$i.'">'.$i.'</a>';
+				}
+			}else{
+				for($i=-NB_PAGE/2+$id; $i<NB_PAGE/2+1+$id; $i++){
+					//On selectionne tous les parametre dans l'url
+						echo '<a href="index.php?page=modif_conf&id='.$i.'">'.$i.'</a>';
+				}
 			}
-			//Milieu
-			echo '...';
-			//La fin
-			for($i=$nb_pages-$page_max; $i<$nb_pages; $i++){
-				//On selectionne tous les parametre dans l'url
-					echo '<a href="index.php?page=modif_conf&id='.$i.'">'.$i.'</a>';
-			}
+			echo '<a href="index.php?page=modif_conf&id='.$nb_pages.'"> >></a>';
 		//FIN ESSAI DE PAGINATION
 	}
 ?>
