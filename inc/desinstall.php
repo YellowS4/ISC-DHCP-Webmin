5e7461757f3bd111a159018531fca30a77e1afc5
AUDON Florian <florian.audon@hotmail.fr>
2014-06-20 11:02:37 +0200

    Correctif

--- inc/desinstall.php
+++ inc/desinstall.php
@@ -4,7 +4,8 @@ ob_start(); /* On initialise le tampon. */
 // On desinstalle le serveur DHCP
 	echo 'Serveur DHCP desinstalle';
 	$desinstalle=shell_exec("scripts/uninstall_dhcp.sh 2>&1");
-	header('Location: index.php');
+	//header('Location: index.php');
+	echo $desinstalle;
 	ob_end_flush(); /* On vide le tampon et on retourne le contenu au client. */	
 	}else{
 		printErrors(Array("Le serveur n'est pas installé"));
