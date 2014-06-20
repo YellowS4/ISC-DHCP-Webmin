5c0af1edd605f937b3ed4fa72e46970425d16ccc
AUDON Florian <florian.audon@hotmail.fr>
2014-06-20 08:11:36 +0200

    UPDATE

--- inc/modif_conf.php
+++ inc/modif_conf.php
@@ -8,11 +8,10 @@ $connex=connexionBDD();
 
 //Constantes
 define("NB_CONF_PAGE","2");
-define("NB_PAGE","2");
 $nw=get_network();
 //On securise la variable ID
 foreach($_POST as $indice=>$valeur){//On securise les valeurs en mettant par défaut une chaine
-	$_POST[$indice]= htmlspecialchars ($valeur);
+	$_POST[$indice]= htmlspecialchars ($valeur,ENT_QUOTES);
 	settype($_POST[$indice], "string");
 }
 if(isset($_POST['conf'])){
@@ -28,71 +27,72 @@ if(isset($_GET['id']) && $_GET['id']>=0 && $_GET['id']!==""){//Sinon "" est comm
 $manquant="";
 
 	if(isset($_POST['ajouter']) && $_POST['ajouter']==="Ajouter"){ //On ajoute la nouvelle configuration
-		if(!isset($_POST['conf_contenu']) || $_POST['conf_contenu']=="" || !isset($_POST['interface']) || $_POST['interface']=="" || !isset($_POST['nom']) || $_POST['nom']=="" || !isset($_POST['actuelle']) || $_POST['actuelle']==""){ //On verifie tous les parametre
-			$manquant="veuillez remplir tous les champs";
+		$erreur="";//Pour mettre des erreurs
+		if(!isset($_POST['contenuconf']) || $_POST['contenuconf']=="" || !isset($_POST['createur']) || $_POST['createur']=="" || !isset($_POST['actuelle']) || $_POST['actuelle']==""){ //On verifie tous les parametre
+			$erreur.="veuillez remplir tous les champs";
+			echo $_POST['createur'];
 		}else{
-			$erreur="";//Pour mettre des erreurs
 			if($_POST['actuelle']=="true"){//Si il existe déjà une conf actuelle on la met en false
-				$resultats=listerConf_actuelle($connex,"true");
-				$donnees=$resultats->fetch();
-				//$resultats=$connex->exec("UPDATE dhcp_test SET conf_actuelle=FALSE WHERE id=".$donnees['id']."; ");	
-				appliquer_conf($_POST['conf_contenu'],$_POST['interface']);
-			}
-			if(isset($erreur) && $erreur!=""){
-				echo $erreur;
-			}else{
-				echo 'Ajout effectue';
-				$resultats=$connex->exec("INSERT INTO dhcp_test (conf_contenu, interface, nom_conf, date_creation, conf_actuelle) VALUES ('".$_POST['conf_contenu']."','".$_POST['interface']."','".$_POST['nom']."','".date("Y-m-d")."','".$_POST['actuelle']."'); ");
-				
+				//a del $resultats=listerConf_actuelle($connex,"true");
+				//a del $donnees=$resultats->fetch();
+				//$resultats=$connex->exec("UPDATE projet34_configurations  SET conf_actuelle=FALSE WHERE id=".$donnees['id']."; ");	
+				appliquer_conf($_POST['contenuconf']);
 			}
 		}
+		if(isset($erreur) && $erreur!=""){
+			echo 'erreur:'.$erreur;
+		}else{
+			echo 'Ajout effectue';
+			$resultats=$connex->exec("INSERT INTO projet34_configurations  (contenuconf,createurconf) VALUES ('".$_POST['contenuconf']."',1); ");
+		}
+		echo '<form method="POST" action="index.php?page=modif_conf"><input type="submit" name="afficher_tout" value="Retour"> </form>';
+		
+		
 	}elseif(isset($_POST['modifier']) && $_POST['modifier']==="Modifier"){//Pour modifier une conf
-		if(!isset($_POST['conf_contenu'])  || $_POST['conf_contenu']=="" || !isset($_POST['interface']) || $_POST['interface']=="" || !isset($_POST['nom']) || $_POST['nom']==""  || !isset($_POST['actuelle']) || $_POST['actuelle']==""){ //On verifie tous les parametre
+		if(!isset($_POST['contenuconf'])  || $_POST['contenuconf']=="" ||  !isset($_POST['createur']) || $_POST['createur']==""  || !isset($_POST['actuelle']) || $_POST['actuelle']==""){ //On verifie tous les parametre
 			$manquant="veuillez remplir tous les champs";
 			echo $manquant;
-			echo '<form method="POST"><input type="submit" value="retour" name="affichage_simple"></form>';
 		}else{
 			$erreur="";//Si toutesfois il y a des erreurs
 			if($_POST['actuelle']=="true"){
 				$resultats=listerConf_actuelle($connex,"true");
 				$donnees=$resultats->fetch();
-				//$resultats=$connex->exec("UPDATE dhcp_test SET conf_actuelle=FALSE WHERE id=".$donnees['id']."; ");	
-				$verif=appliquer_conf($_POST['conf_contenu'],$_POST['interface']);
+				//$resultats=$connex->exec("UPDATE projet34_configurations  SET conf_actuelle=FALSE WHERE id=".$donnees['id']."; ");	
+				$verif=appliquer_conf($_POST['contenuconf']);
 				if($verif!=""){
 					$erreur.="Erreur dans la configuration: ".$verif."\n";
-					echo '<form method="POST"><input type="submit" value="retour" name="affichage_simple"></form>';
 				}else{
-					echo "Configuration appilqué ";
+					echo "Configuration appliqué ";
 				}
 			}
 			if(isset($erreur) && $erreur!=""){
 				echo $erreur;
 			}else{
 				echo 'modification effectue';
-				//$resultats=$connex->exec("UPDATE dhcp_test SET conf_contenu='".$_POST['conf_contenu']."', interface='".$_POST['interface']."', nom_conf='".$_POST['nom']."', conf_default='".$_POST['default']."', conf_actuelle='".$_POST['actuelle']."' WHERE id='".$_POST['id']."'; ");
+				$resultats=$connex->exec("UPDATE projet34_configurations  SET contenuconf='".$_POST['contenuconf']."', createurconf=1 WHERE idconf='".$_POST['id']."'; ");
 			}
 		}
+		echo '<form method="POST" action="index.php?page=modif_conf"><input type="submit" name="afficher_tout" value="Retour"> </form>';		
 	}elseif(isset($_POST['afficher_une_conf']) && $_POST['afficher_une_conf']==="ok"){//Pour afficher qu'une seul conf depuis l'affichage simple
-		//$conf=$connex->query("SELECT id,conf_contenu, interface, nom_conf, date_creation, conf_default, conf_actuelle FROM dhcp_test WHERE id=".$_POST['conf'].";");
+		//$conf=$connex->query("SELECT id,contenuconf, interface, nom_conf, creation, conf_default, conf_actuelle FROM projet34_configurations  WHERE id=".$_POST['conf'].";");
 		$conf=listerConf_id($connex,$_POST['conf']);
 		$row=$conf->fetch();
 		?>
 		<form method="POST" action=""> 
 				<input type="hidden" name="id" value="<?php echo $row['id'];?>">
-				<label class="aligner"><span style="vertical-align:top;">fichier dhcpd.conf: </span></label><textarea name="conf_contenu" rows="20" cols="50"><?php echo $row['conf_contenu'];?></textarea><br />	  
-				<label class="aligner">nom de la configuration: </label><input type="text" name="nom" value="<?php echo $row['nom_conf'];?>"/><br />
+				<label class="aligner"><span style="vertical-align:top;">fichier dhcpd.conf: </span></label><textarea name="contenuconf" rows="20" cols="50"><?php echo $row['contenuconf'];?></textarea><br />	  
+				<label class="aligner">Propriétaire de la configuration: </label><input type="text" name="nom" value="<?php echo $row['nomuser'];?>" readonly="readonly"/><br />
 				<label class="aligner">Appliquer la configuration (cela remplaçera la configuration actuelle):<label>  Oui</label> <input type="radio" name="actuelle" value="true" <?php if($row['conf_actuelle']=="true"){ echo ' checked="checked"';} ?>></label> <label>Non: <input type="radio" name="actuelle" value="false"<?php if($row['conf_actuelle']!="true"){ echo ' checked="checked"';} ?>></label><br />
 				<input type="submit" value="Ajouter" name="ajouter">
 				<input type="submit" value="Modifier" name="modifier">
 				<input type="submit" value="retour" name="affichage_simple">
 			</form>	
 			<?php
-	}elseif((isset($_POST['afficher_tout']) && $_POST['afficher_tout']==="Affichage détaillé") || (isset($_GET['id']) && $_GET['id']!=="" && !isset($_POST['affichage_simple'])) || (isset($_POST['selectionner']) && $_POST['selectionner']==="selectionner") || (isset($_POST['trie']) && $_POST['trie']==="trié")){
+	}elseif((isset($_POST['afficher_tout']) && ($_POST['afficher_tout']==="Affichage détaillé" || $_POST['afficher_tout']==="Retour")) || (isset($_GET['id']) && $_GET['id']!=="" && !isset($_POST['affichage_simple'])) || (isset($_POST['selectionner']) && $_POST['selectionner']==="selectionner") || (isset($_POST['trie']) && $_POST['trie']==="trié")){
 		//Tri
 		print 'Afficher seulement:<form method="POST" class="liste_conf"> Les configuration de l\'utilisateur: Après la date:<input type="date" name="selection_date"><input type="submit" value="selectionner" name="selectionner">';
 		print 'Trié par date <select name="trie_date"><option value="ASC">Croissant</option><option value="DESC">Descroissant</option></select><input type="submit" value="trié" name="trie"><br /></form>';
 		
-		echo $_GET['id'];
 		//On prends le nombre de configuration pour la pagination
 		if(isset($_POST['selection_date']) && $_POST['selection_date']!==""){
 			$liste_conf=listerConf($connex,"0","0",$_POST['selection_date']);
@@ -105,7 +105,7 @@ $manquant="";
 			if($nb_conf%NB_CONF_PAGE==0){
 				$nb_pages=$nb_conf/NB_CONF_PAGE;
 			}else{
-				$nb_pages=floor($nb_conf/NB_CONF_PAGE);
+				$nb_pages=ceil($nb_conf/NB_CONF_PAGE);
 			}
 			//On verifie les deux valeurs pour le trie DESC ou ASC 
 			if(isset($_POST['trie_date']) && ($_POST['trie_date']==="ASC" || $_POST['trie_date']==="DESC")){
@@ -130,11 +130,11 @@ $manquant="";
 				
 			?>
 			
-				<form method="POST" action="" class="conf<?php if($row['conf_actuelle']==="true"){ echo '_actuelle';} //On ajoute toutes nos classes pour les gerer en js ?>"> 
-					<input type="hidden" name="id" value="<?php echo $row['id'];?>">
-					<label class="aligner"><span style="vertical-align:top;">fichier dhcpd.conf: </label></span><textarea class="form" name="conf_contenu" rows="20" cols="50"><?php echo $row['conf_contenu'];?></textarea><br />				  
-					<label class="aligner">nom de la configuration: </label><input type="text" class="form" name="nom" value="<?php echo $row['nom_conf'];?>"/><br />
-					Appliquer la configuration (cela remplaçera la configuration actuelle):  <label>Oui<input type="radio" name="actuelle" value="true" <?php if($row['conf_actuelle']=="true"){ echo ' checked="checked"';} ?>></label>  <label>Non: <input type="radio" name="actuelle" value="false"<?php if($row['conf_actuelle']!="true"){ echo ' checked="checked"';} ?>></label><br />
+				<form method="POST" action="" class="conf"> 
+					<input type="hidden" name="id" value="<?php echo $row['idconf'];?>">
+					<label class="aligner"><span style="vertical-align:top;">fichier dhcpd.conf: </label></span><textarea class="form" name="contenuconf" rows="20" cols="50"><?php echo $row['contenuconf'];?></textarea><br />				  
+					<label class="aligner">Propriétaire de la configuration: </label><input type="text" class="form" name="createur" value="<?php echo $row['nomuser'];?>" readonly="readonly"/><br />
+					Appliquer la configuration (cela remplaçera la configuration actuelle):  <label>Oui<input type="radio" name="actuelle" value="true"></label>  <label>Non: <input type="radio" name="actuelle" value="false" checked="checked"></label><br />
 					<input type="submit" value="Ajouter" name="ajouter">
 					<input type="submit" value="Modifier" name="modifier">
 				</form>	
@@ -143,37 +143,34 @@ $manquant="";
 			
 			//ESSAI DE PAGINATION
 			//On definit les pages a afficher
+			echo $nb_pages;
+			if($nb_pages>=2 && $nb_pages<=4){//Si il n'y a pas beaucoup de pages
+			
+				for($i=0; $i<$nb_pages; $i++){
+					//	echo $nb_pages;
+						echo '<a href="?page=modif_conf&id='.$i.'">'.$i.'</a>';
+					}
+			}elseif($nb_pages >4){
 				//Le debut
-				echo '<a href="modif_conf-0.php"><< </a>';
-				if($id>=6){
-					for($i=-NB_PAGE/2+5; $i<NB_PAGE/2+6; $i++){
-						//On selectionne tous les parametre dans l'url
-							echo '<a href="modif_conf-'.$i.'.php">'.$i.'</a>';
-					}
-				}elseif($id<=0){
-					for($i=-NB_PAGE/2+1; $i<NB_PAGE/2+2; $i++){
-						//On selectionne tous les parametre dans l'url
-							echo '<a href="modif_conf-'.$i.'.php">'.$i.'</a>';
-					}
-				}else{
-					for($i=-NB_PAGE/2+$id; $i<NB_PAGE/2+1+$id; $i++){
-						//On selectionne tous les parametre dans l'url
-							echo '<a href="modif_conf-'.$i.'.php">'.$i.'</a>';
-					}
+				echo '<a href="?page=modif_conf&id=0"><< </a>';
+				$milieu=ceil($nb_pages/2);
+				for($i=$milieu-1;$i<=$milieu+1;$i++){
+					echo '<a href="?page=modif_conf&id='.$i.'">'.$i.'</a>';
 				}
-				echo '<a href="modif_conf-'.$nb_pages.'.php"> >></a>';
+				echo '<a href="?page=modif_conf&id='.($nb_pages-1).'"> >></a>';
 			//FIN ESSAI DE PAGINATION
 			}
+		}
 		print '<form method="POST" action=""><input type="submit" name="affichage_simple" value="Affichage simple"> </form>';
 	}else{
 		$liste_conf=listerConf($connex);
 		print '<form method="POST" action="">';
 		print 'Listes configurations: <select name="conf">';
 				foreach($liste_conf as $ligne){
-					print '<option value="'.$ligne['id'].'">'.$ligne['date_creation'].'('.$ligne['nom_conf'].')</option>'."\n";
+					print '<option value="'.$ligne['idconf'].'">'.substr($ligne['creation'],'0','19').'('.$ligne['nomuser'].')</option>'."\n";
 				}
 				print '</select><input type="submit" name="afficher_une_conf" value="ok"> </form>';
-		print '<form method="POST" action=""><input type="submit" name="afficher_tout" value="Affichage détaillé"> </form>';
+		print '<form method="POST" action="index.php?page=modif_conf"><input type="submit" name="afficher_tout" value="Affichage détaillé"> </form>';
 	}
 ?>
 </article>
